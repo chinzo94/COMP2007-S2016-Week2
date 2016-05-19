b84db3dae84edbf3bf328363789bed72a7290f55
chinzo94 <chado94@rogers.com>
2016-05-19 19:03:18 -0400

    add active classes to navbar

--- COMP2007-S2016-Week2/Site.Master.cs
+++ COMP2007-S2016-Week2/Site.Master.cs
@@ -11,7 +11,34 @@ namespace COMP2007_S2016_Week2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            addActiveClass();
+        }
 
+        /**
+         * Adds class "active" to list items in navbar
+         * 
+         * @method addActiveClass
+         */
+        private void addActiveClass()
+        {
+            switch (Page.Title)
+            {
+                case "Home Page":
+                    home.Attributes.Add("class", "active");
+                    break;
+                case "Products":
+                    products.Attributes.Add("class", "active");
+                    break;
+                case "Services":
+                    services.Attributes.Add("class", "active");
+                    break;
+                case "About":
+                    about.Attributes.Add("class", "active");
+                    break;
+                case "Contact":
+                    contact.Attributes.Add("class", "active");
+                    break;
+            }
         }
     }
 }
